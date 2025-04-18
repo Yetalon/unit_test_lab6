77e3019462b9becae09541a26433118ca06db37b
Yetalon <Davisac7@etsu.edu>
2025-04-18 15:49:43 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace unit_test_lab6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ unit_test_lab6.Tests/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace unit_test_lab6.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
